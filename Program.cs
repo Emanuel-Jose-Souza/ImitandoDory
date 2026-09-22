91bf457326b817aba4f7b3740b83217d6342da98
DESKTOP-4A7G64Q\User <user@etec.com>
2026-09-22 11:25:45 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿string palavra = Console.ReadLine()!;
+string palavra01 = palavra.Replace("a", "aaa");
+string palavra02 = palavra01.Replace("e", "eee");
+string palavra03 = palavra02.Replace("i", "iii");
+string palavra04 = palavra03.Replace("o", "ooo");
+string palavra05 = palavra04.Replace("u", "uuu");
+
+string palavra06 = palavra05.Replace("A", "AAA");
+string palavra07 = palavra06.Replace("E", "EEE");
+string palavra08 = palavra07.Replace("I", "III");
+string palavra09 = palavra08.Replace("O", "OOO");
+string palavra010 = palavra09.Replace("U", "UUU");
+
+
+Console.WriteLine(palavra010);
